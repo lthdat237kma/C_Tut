11f3e1c0b53edbcd510151c9d49f6a16ff0026a6
Le Thanh Dat <130472589+lthdat237kma@users.noreply.github.com>
2025-02-13 21:06:22 +0700

    Add files via upload

---
+++ 13_02_2025_BT1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+	
+	int n;
+	printf("Mang co so phan tu la: ");
+	scanf("%i", &n);
+	
+	if (n <= 0) { 
+	printf("Mang khong hop le!");
+	}
+	
+	else { 
+	int a[n];
+	printf("Nhap %i phan tu cua mang: \n\n", n);
+
+	for (int i = 0; i < n; i++)  {
+        printf("Phan tu thu %d la: \n", i);
+        scanf("%d", &a[i]);
+    }
+    
+	int max = a[0];
+    for (int i = 1; i < n; i++) {
+        if ( a[i] > max) {
+            max = a[i];
+        }
+    }
+    printf("Phan tu lon nhat trong mang da nhap la: %i", max);
+	}
+}
+	
